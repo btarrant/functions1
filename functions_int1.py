435ac797a9286ddee9b423974fd29e261b94840f
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-10 15:02:40 -0500

    Iterate through a list of dictionaries.

--- functions_int1.py
+++ functions_int1.py
@@ -25,3 +25,22 @@ print( sports_directory['soccer'])
 # 4.
 z[0]['y'] = 30
 print(z)
+
+
+# Iterate Through A List Of Dictionaries
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterate_dictionary(list):
+    for i in range(0, len(list)):
+        output = ""
+        for key,val in list[i].items():
+            output += f" {key} - {val},"
+        print(output)
+
+iterate_dictionary(students)
